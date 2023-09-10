e28e5b7a698620c80c51ad65faa553a7d37f6ff2
suparnaneeraj <suparnaneeeraj94@gmail.com>
2023-09-10 18:31:12 +0200

    changed data

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -6,7 +6,7 @@ describe('Products Functionality', () => {
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
         cy.location('pathname').should('eq', '/v1/inventory.html');  ///to check if webpage is /v1/inventory.html
-        cy.get('.inventory_item_name').contains('Sauce Labs Backpack').parents('.inventory_item').contains('ADD TO CART').click()
+        cy.get('.inventory_item_name').contains('Sauce Labs Bike Light').parents('.inventory_item').contains('ADD TO CART').click()
 
     })
     it('should allow selection between pages', () => {
